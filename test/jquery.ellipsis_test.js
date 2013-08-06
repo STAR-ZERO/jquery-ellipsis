0425bd03e77443b49189b8e72ab074eac9e4179e
STAR_ZERO <kenji.01.star@gmail.com>
2013-08-07 00:05:26 +0900

    Support Grunt v0.4

--- test/jquery.ellipsis_test.js
+++ test/jquery.ellipsis_test.js
@@ -1,6 +1,3 @@
-/*global QUnit:false, module:false, test:false, asyncTest:false, expect:false*/
-/*global start:false, stop:false ok:false, equal:false, notEqual:false, deepEqual:false*/
-/*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
 (function($) {
 
     test('one line ellipsis', function() {
